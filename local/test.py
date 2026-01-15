2fbf88165b63c8a29b4cb057e324c4fca60c0847
TheMoistDino <themoistdino@gmail.com>
2026-01-15 14:01:37 -0800

    Adjusting motor, servo duty cycles

--- local/test.py
+++ local/test.py
@@ -6,8 +6,8 @@ from adafruit_pca9685 import PCA9685
 import numpy as np
 
 parser = argparse.ArgumentParser(description="Motor Testing Script!")
-parser.add_argument('-e', '--esc', type=int, default=1, help='ESC channel (default:1)')
-parser.add_argument('-s', '--servo', type=int, default=0, help='Servo channel (default:0)')
+parser.add_argument('-e', '--esc', type=int, default=14, help='ESC channel (default:14)')
+parser.add_argument('-s', '--servo', type=int, default=15, help='Servo channel (default:15)')
 parser.add_argument('-i', '--initialize', type=int, default = "0", help='Is the motor running for the first time since being switched on? (default: 0)')
 parser.add_argument('--extra_option', type=str, default="", help='Message to explain the option (default: empty string)') #Template for additional arguments
 args = parser.parse_args()
@@ -29,13 +29,29 @@ pca = PCA9685(i2c)
 pca.frequency = FREQ
 
 # === Give time for ESC to initialize ===
+print("Waiting for 2 seconds")
 time.sleep(2)
+print("Starting testing script -- NEUTRAL")
 pca.channels[ESC_CHANNEL].duty_cycle = int(1.5*65535/16.67)
+pca.channels[SERVO_CHANNEL].duty_cycle = int(1.5*65535/16.67)
 time.sleep(2)
 
 # === Run motor testing sequence ===
+print("Starting motor -- RUNNING AT MAX THROTTLE")
 pca.channels[ESC_CHANNEL].duty_cycle = int(1.9*65535/16.67)
 time.sleep(5)
 
+# === Run servo testing sequence ===
+print("Testing servo -- SWEEP LEFT")
+pca.channels[SERVO_CHANNEL].duty_cycle = int(1.2*65535/16.67)
+time.sleep(2)
+print("Testing servo -- SWEEP RIGHT")
+pca.channels[SERVO_CHANNEL].duty_cycle = int(1.8*65535/16.67)
+time.sleep(2)
+print("Testing servo -- BACK TO NEUTRAL")
+pca.channels[SERVO_CHANNEL].duty_cycle = int(1.5*65535/16.67)
+time.sleep(2)
+
 # === Tear down PCA board data structure ===
+print("Stopping board -- BACK TO NEUTRAL")
 pca.deinit()
